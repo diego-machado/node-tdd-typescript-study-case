d4209f7fdf71508ce00a7f672a941e44fdf1fd29
Diego Machado <diego.machadodarosa@gmail.com>
2020-09-02 16:20:08 -0300

    feat: ensure AccountMongoRepository returna an acccount on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helper/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountConnection = MongoHelper.getConnection('accounts')
+    const result = await accountConnection.insertOne(accountData)
+
+    const account = result.ops[0]
+    const { _id, accountWithoutId } = account
+
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
