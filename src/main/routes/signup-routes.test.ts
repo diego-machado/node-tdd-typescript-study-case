91d1fbca7acff656d4c63f44d189fbad92fdefec
Diego Machado <diego.machadodarosa@gmail.com>
2020-09-22 21:41:26 -0300

    test: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,7 +12,7 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getConnection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
