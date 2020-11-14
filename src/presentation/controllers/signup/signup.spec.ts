e40c8d318e3e32702a25ed9043960d6524044fa9
Diego Machado <diego.machadodarosa@gmail.com>
2020-11-14 19:51:19 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -179,7 +179,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('fail'))
   })
 
   test('Should call AddACcount with correct values', async () => {
