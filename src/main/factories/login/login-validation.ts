a55aaaaec1acafa1616fb6e36e7fbf1abb1242a9
Diego Machado <diego.machadodarosa@gmail.com>
2020-11-23 22:14:29 -0300

    refactor: group validation imports in a single file

--- src/main/factories/login/login-validation.ts
+++ src/main/factories/login/login-validation.ts
@@ -1,8 +1,7 @@
 
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validators/validation'
+import { EmailValidation, RequiredFieldValidation } from '../../../presentation/helpers/validators'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
