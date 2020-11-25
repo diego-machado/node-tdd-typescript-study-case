2c4b1b0b4aa1f6e4e5a5f81e25d2ead10717e8f3
Diego Machado <diego.machadodarosa@gmail.com>
2020-11-24 21:04:47 -0300

    refactor: move EmailValidatorAdaptar to the main layer

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -2,7 +2,7 @@
 import { EmailValidation, RequiredFieldValidation } from '../../../presentation/helpers/validators'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeSignUpController } from '../factories/signup/signup'
-import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
