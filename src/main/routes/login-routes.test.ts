f9c9648849e7a7ad9881964807809648cad16c3f
Diego Machado <diego.machadodarosa@gmail.com>
2020-11-25 15:52:18 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'diego.machadodarosa@gmail.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 })
